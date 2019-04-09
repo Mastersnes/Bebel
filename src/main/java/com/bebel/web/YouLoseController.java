88f2d32ac32914d2f592c965bace8fefda449e20
Mastersnes <steven.nesztler@gmail.com>
2019-04-09 18:51:19 +0200

    repo embarqué

--- src/main/java/com/bebel/web/YouLoseController.java
+++ src/main/java/com/bebel/web/YouLoseController.java
@@ -3,28 +3,27 @@ package com.bebel.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import request.SaveRequest;
 
 @Controller
 @RequestMapping("/youlose")
 public class YouLoseController {
 
-  @GetMapping("/getSaves")
-  public ResponseEntity<String> getSave(@RequestParam String username) {
-    return new ResponseEntity<>("Voici les sauvegarde de " + username, HttpStatus.OK);
+  @GetMapping("/save/{username}/{type}")
+  @ResponseBody
+  public ResponseEntity<String> getSave(@PathVariable("username") final String username,
+                                        @PathVariable(value = "type", required = false) final String type) {
+    if (type == null) {
+      return new ResponseEntity<>("Voici les sauvegardes de " + username, HttpStatus.OK);
+    }else {
+      return new ResponseEntity<>("Ceci est la sauvegarde " + type + " de " + username, HttpStatus.OK);
+    }
   }
 
-  @GetMapping("/getSave")
-  public ResponseEntity<String> getSave(@RequestParam String username, @RequestParam String type) {
-    return new ResponseEntity<>("Ceci est la sauvegarde " + type + " de " + username, HttpStatus.OK);
-  }
-
-  @PostMapping("/save")
-  public ResponseEntity save(@RequestParam final SaveRequest request) {
+  @PostMapping(value = "/save")
+  @ResponseBody
+  public ResponseEntity<String> save(@RequestBody final SaveRequest request) {
     return new ResponseEntity<>("enregistrement de la sauvegarde " + request.getData() + "de type :" + request.getType() + " pour " + request.getUsername(), HttpStatus.CREATED);
   }
 }
